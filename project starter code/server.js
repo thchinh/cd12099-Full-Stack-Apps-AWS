3dd4d7ed820005a2b525b7a12348331d243dc693
Hữu Chinh <khleson79929@gmail.com>
2024-03-31 20:59:09 +0700

    done project

---
+++ project starter code/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {
+  filterImageFromURL,
+  isValidUrl,
+} from "./util/util.js";
+
+// Init the Express application
+const app = express();
+
+// Set the network port
+const port = process.env.PORT || 8082;
+
+// Use the body parser middleware for post requests
+app.use(bodyParser.json());
+
+app.get("/", async (req, res) => {
+  res.send("try GET /filteredimage?image_url={{}}");
+});
+
+app.get("/filteredimage", async (req, res) => {
+  const { image_url } = req.query;
+
+  if (!image_url || !isValidUrl(image_url))
+    return res
+      .status(400)
+      .json({errors: ["Image url is invalid, please check again!."]});
+
+  const filePath = await filterImageFromURL(image_url);
+
+  return res.status(200).json({ filePath });
+});
+
+// Start the Server
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
---
+++ project starter code/util/util.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import fs from "fs";
+import Jimp from "jimp";
+import { URL } from "url";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export async function filterImageFromURL(inputURL) {
+  const outPath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data: imageBuffer } = await axios({
+        method: "get",
+        url: inputURL,
+        responseType: "arraybuffer",
+      });
+
+      const photo = await Jimp.read(imageBuffer);
+      photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname + outPath, (img) => {
+          resolve(outPath);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export async function deleteLocalFiles(files) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
+}
+
+export const isValidUrl = (s) => {
+  try {
+    new URL(s);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
